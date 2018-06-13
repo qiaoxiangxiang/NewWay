08078f777defebf87392f8dbd5b66b09078933f0
yk-dream <git@125.35.5.233:23585>
2018-06-13 18:47:09 +0800

    123

---
+++ js/index.js
@@ -0,0 +1,47 @@
+$(".box_top").append(`
+<div class="top">
+<div class="top_box">
+    <div class="logo">
+        <img class="logo_img" src="../../img/index/top_02.png" alt="">
+    </div>
+    <!-- 一级nav -->
+    <ul class="nav">
+        <li class="nav_li">首页</li>
+        <li class="nav_li">教育服务</li>
+        <li class="nav_li">示范案例</li>
+        <li class="nav_li">教育研究</li>
+        <li class="nav_li">技能竞赛</li>
+        <li class="nav_li">新闻动态</li>
+        <li class="nav_li">合作与生态</li>
+        <li class="nav_li">关于新道</li>
+    </ul>
+</div>
+<!-- 一级 content -->
+<ul class="nav_box">
+    <li class="nav_content_box">
+
+    </li>
+    <li class="nav_content_box">
+
+    </li>
+    <li class="nav_content_box">
+
+    </li>
+    <li class="nav_content_box">
+
+    </li>
+    <li class="nav_content_box">
+
+    </li>
+    <li class="nav_content_box">
+
+    </li>
+    <li class="nav_content_box">
+
+    </li>
+    <li class="nav_content_box">
+
+    </li>
+</ul>
+</div>
+`)
